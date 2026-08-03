7526b891ad4c1b6a64eda98d5fe42bcf32f8b5ad
Tomas Neubauer <tomas@quix.ai>
2026-08-03 08:54:50 +0000

    Update file QuixLabNotebooks/quixlab/main.py

--- QuixLabNotebooks/quixlab/main.py
+++ QuixLabNotebooks/quixlab/main.py
@@ -3,7 +3,7 @@ import quixlab as ql
 canvas = ql.Canvas(title="My Notebook", lake_tree_open=['billing_events', 'billing_events/environment_id=testrigorg-ingestionpipelineforreal-6deb6d8f'])
 
 
-@canvas.dataset(position=(365, 128), size=(560, 420), code_height=200)
+@canvas.dataset(position=(365, 128), size=(706, 471), code_height=200)
 def billing_events():
     return ql.sql("""SELECT *
     FROM billing_events
