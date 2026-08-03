88c5e371f94698dc06004d0f8afa9ee803ec0fe4
Tomas Neubauer <tomas@quix.ai>
2026-08-03 09:45:15 +0000

    Edited 'main.py'

--- quixlab/main.py
+++ quixlab/main.py
@@ -1,1 +1,24 @@
+import quixlab as ql
 
+canvas = ql.Canvas(title="My Notebook", lake_tree_open=['billing_events', 'billing_events/environment_id=testrigorg-ingestionpipelineforreal-6deb6d8f'])
+
+
+@canvas.dataset(position=(365, 128), size=(712, 651), code_height=200)
+def billing_events():
+    return ql.sql("""SELECT *
+    FROM billing_events
+    WHERE environment_id = 'testrigorg-ingestionpipelineforreal-6deb6d8f'
+    ORDER BY event_datetime
+    """)
+
+
+    # ql-ai-mode: agent
+
+
+@canvas.datastore(position=(1303, 90), size=(560, 420), code_height=120, viz={'datastore': True, 'sourceNode': 'ai_1'})
+def ai_1_store(ai_1):
+    return ql.datastore("ai_1_store")
+
+
+if __name__ == "__main__":
+    canvas.serve()
